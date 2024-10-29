b824312f6290ef1e5a8f199da230fc8252cc8705
Laptop <laptop@disroot.org>
2024-10-29 22:49:39 +0200

    tidy css; put the player script into a file instead of rendering it in the page

---
+++ assets/player.js
@@ -0,0 +1,8 @@
+var audio = document.getElementById('track');
+if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(audio.src);
+    hls.attachMedia(audio);
+} else if (!audio.canPlayType('application/vnd.apple.mpegurl')) {
+    alert('HLS is not supported! Audio playback will not work.');
+}
