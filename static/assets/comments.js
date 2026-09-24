bc050491611afa3399650ba0093d37cf6142fc38
laptopcat <laptop@maid.zone>
2026-09-24 15:22:17 +0300

    maybe fix comment seek sometimes

--- static/assets/comments.js
+++ static/assets/comments.js
@@ -1,15 +1,18 @@
 var comm = document.getElementById('comments');
 var in_flight = false;
+if (!audio) {
+    var audio = document.getElementById("track");
+}
 function comments(self) {
-    if (in_flight) return; 
+    if (in_flight) return;
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/_/partials/comments/'+self.getAttribute('data-id')+self.getAttribute('href'), true);
-    xhr.onerror = function(e) {
+    xhr.open('GET', '/_/partials/comments/' + self.getAttribute('data-id') + self.getAttribute('href'), true);
+    xhr.onerror = function (e) {
         alert('Something went wrong. Check console');
         console.error(e);
         in_flight = false;
     }
-    xhr.onload = function() {
+    xhr.onload = function () {
         if (xhr.status != 200) {
             alert(xhr.responseText);
             return;
@@ -23,13 +26,34 @@ function comments(self) {
         }
         self.setAttribute('href', next);
         self.textContent = 'more comments';
-        comm.querySelectorAll('.link[data-timestamp]').forEach(function(el) {
-			el.onclick = function() {
-                if (!audio) var audio = document.getElementById("track");
-				audio.currentTime = el.dataset.timestamp / 1000;
-				audio.play();
-			};
-		});
+        comm.querySelectorAll('.link[data-timestamp]').forEach(function (el) {
+            el.onclick = function (event) {
+                event.preventDefault();
+                var ts = el.dataset.timestamp / 1000;
+                if (window.Hls) {
+                    audio.play();
+                    audio.currentTime = ts;
+                } else {
+                    // fix for restream aac on firefox as it's fmp4 but doesn't work on chrome \(^-^)/
+                    if (audio.duration < ts) {
+                        audio.ondurationchange = function () {
+                            if (audio.currentTime < ts) {
+                                audio.currentTime = ts;
+                            }
+                            if (audio.currentTime >= ts) {
+                                audio.ondurationchange = undefined;
+                                audio.currentTime = ts;
+                                audio.play();
+                            }
+                        }
+                        audio.currentTime = ts;
+                    } else {
+                        audio.currentTime = ts;
+                        audio.play();
+                    }
+                }
+            };
+        });
         in_flight = false;
     }
     in_flight = true;
